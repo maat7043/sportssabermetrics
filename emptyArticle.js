12910e48fa8d50bc0da10cc16e1a2c67a6d3e57e
Matt <maatspencer@gmail.com>
2018-03-20 05:25:37 -0400

    Add files via upload

---
+++ emptyArticle.js
@@ -0,0 +1,24 @@
+var coverImage, title, shortTitle, author, dateString, dateNumerical, articleID, articleBody, articleIntro;
+
+// Article 
+coverImage = 'rep01';
+shortTitle = 'rep02';
+title = 'rep03';
+author = 'rep04';
+dateString = 'rep05'; //Written format
+dateNumerical = 'rep06'; // mm-dd-yy
+articleID = 'rep07';
+articleBody = 'rep08';
+articleIntro = 'rep9';
+
+var article0 = {
+	coverImage: coverImage,
+	title: title,
+	shortTitle: shortTitle,
+	author:author, 
+	dateString: dateString,
+	dateNumerical: dateNumerical,
+	articleID: articleID,
+	articleBody: articleBody,
+	articleIntro: articleIntro
+};
